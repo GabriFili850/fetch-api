2b5ec05ee1a51b9a47ecdb24a964f30e25d5b4b9
GabriFili850 <gabriele.filigheddu@live.it>
2022-09-23 16:36:37 +0100

    add axios, fetch data, render data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const { UseEffect, useState } = React;
+
+const fetchRandomData = () => {
+  return axios
+    .get("https://randomuser.me/api")
+    .then(({ data }) => {
+      // handle success
+      console.log(data);
+      return JSON.stringify(data);
+    })
+    .catch((err) => {
+      // handle error
+      console.error(err);
+    });
+};
+
+const App = () => {
+  const [randomUserDataJSON, setRandomUserDataJSON] = useState("");
+
+  useEffect(() => {
+    fetchRandomData().then((randomData) => {
+      setRandomUserDataJSON(randomData);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This App will fetch random data from an API</h1>
+
+      <button
+        onClick={() => {
+          fetchRandomData();
+        }}
+      >
+        Fetch Random Data
+      </button>
+      <p>{randomUserDataJSON}</p>
     </div>
   );
-}
+};
 
 export default App;
